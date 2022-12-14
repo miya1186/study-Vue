e3fd1082f717f88c2cf991d27fdc7bb0e74d9001
miya1186 <miyamiya1186@gmail.com>
2022-12-14 21:56:17 +0900

    data binding

--- js/hello.js
+++ js/hello.js
@@ -1,6 +1,14 @@
+// createAppの戻り値はVue.jsの機能を提供するVueインスタンス
 const app = Vue.createApp({
   data: () => ({
-    message: "Hello Vue.js!!",
+    message: "Hello Vue3.js!!",
+    user: {
+      name: "miya",
+      age: 27,
+    },
+    colors: ["red", "green"],
   }),
 });
+
+// 指定されたHTML要素（id=app)がVue.jsが構築したDOMに置き換えられる＝マウント
 app.mount("#app");
